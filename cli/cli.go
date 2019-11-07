bd9e53fe3edd769931fe3e558f0b793b32d293eb
Loric Brevet <loric.brevet@gmail.com>
2019-11-07 21:58:00 +0100

    Refactor almost entirely cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,8 +1,8 @@
 package cli
 
 import (
-	"errors"
 	"flag"
+	"fmt"
 	"os"
 )
 
@@ -10,119 +10,125 @@ var ErrorHandling = flag.ExitOnError
 
 type Action struct {
 	Name string
-	Run  func()
-
-	flagset *flag.FlagSet
+	Run  func(fs *flag.FlagSet) error
+	fs   *flag.FlagSet
 }
 
-func (a *Action) FlagSet() *flag.FlagSet {
-	if a.flagset == nil {
-		a.flagset = flag.NewFlagSet(a.Name, ErrorHandling)
+func (act *Action) Flags() *flag.FlagSet {
+	if act.fs == nil {
+		act.fs = flag.NewFlagSet(act.Name, ErrorHandling)
 	}
-
-	return a.flagset
+	return act.fs
 }
 
 type Object struct {
-	Name string
-
+	Name    string
 	actions map[string]*Action
-	flagset *flag.FlagSet
 }
 
-// SetAction will add or replace an cli action.
+// AddAction will add or replace an cli action.
 // The action name is taken as the unique ID so it should be provided.
-func (o *Object) SetAction(a *Action) error {
-	if a == nil {
-		return errors.New("cannot add nil action")
+// If the action does not have any name, it will replace the default action.
+// If the action is nil, it will panic
+func (obj *Object) AddAction(act *Action) {
+	if act == nil {
+		panic("cannot add nil action")
 	}
-	if a.Name == "" {
-		return errors.New("cannot add action with empty name")
-	}
-	o.actions[a.Name] = a
+
+	obj.actions[act.Name] = act
 }
 
-func (o *Object) FlagSet() *flag.FlagSet {
-	if o.flagset == nil {
-		o.flagset = flag.NewFlagSet(o.Name, ErrorHandling)
-	}
-
-	return o.flagset
+func (obj *Object) RootAction() *Action {
+	return obj.actions[""]
 }
 
 type App struct {
-	Name string
-
-	root    Object
+	Name    string
 	objects map[string]*Object
 }
 
 func New() *App {
 	var app App
-	app.root = Object{}
+
+	app.objects = make(map[string]*Object)
+	app.AddObject(&Object{})
+
 	return &app
 }
 
-// SetObject will add or replace an cli object.
-// The object name is taken as the unique ID so it should be provided.
-func (app *App) SetObject(obj *Object) error {
+// TODO: to implement
+func (app *App) Usage() string {
+	return "usage"
+}
+
+func (app *App) RootObject() *Object {
+	return app.objects[""]
+}
+
+// AddObject will add or replace an cli object.
+// If the object does not have any name, it will replace the default object.
+// If the object is nil, it will panic
+func (app *App) AddObject(obj *Object) {
 	if obj == nil {
-		return errors.New("cannot add nil object")
+		panic("cannot add nil object")
 	}
-	if obj.Name == "" {
-		return errors.New("cannot add object with empty name")
+
+	if obj.actions == nil {
+		obj.actions = make(map[string]*Action)
 	}
+
+	if _, ok := obj.actions[""]; !ok || obj.actions[""].Run == nil {
+		obj.actions[""].Run = app.defaultRun
+	}
+
 	app.objects[obj.Name] = obj
 }
 
-// SetAction will add or replace an cli action of the default object.
-// The action name is taken as the unique ID so it should be provided.
-func (app *App) SetAction(a *Action) error {
-	return app.root.SetAction(a)
-}
-
-func (app *App) FlagSet() *flag.FlagSet {
-	return app.root.FlagSet()
-}
-
-func (app *App) Parse() error {
-	if len(os.Args) < 2 || isFlag(os.Args[1]) {
-		return errors.New("expected at least action in command")
-	}
-
-	// TODO: use these flag
-	var hasObj bool
-	var actionIdx int = 1
-
-	// Rewrite in order to set the action as optional (because of myapp --version for instance)
-
-	for i := 1; i < len(os.Args); i++ {
-		arg := os.Args[i]
-
-		if !isFlag(arg) {
-			if actionIdx == 0 {
-				actionIdx = i
-				break
-			}
-			if objectIdx == 0 {
-				objectIdx = actionIdx
-				actionIdx = i
-				break
-			}
-		}
-	}
-
-	if actionIdx == 0 {
-		return errors.New("expected at least action in command")
-	}
-
-	if objectIdx == 0 {
-		//
-	} else if _, ok := app.objects[os.Args[1]]; ok {
-		// this feels wrong because objectIdx is useless
-	}
-
+func (app *App) defaultRun(fs *flag.FlagSet) error {
+	fmt.Println(app.Usage())
 	return nil
+}
+
+func (app *App) Run() {
+	var obj, act string
+	var flagsIdx int = 1
+
+	switch {
+	case len(os.Args) >= 3 && !isFlag(os.Args[1]) && !isFlag(os.Args[2]):
+		obj = os.Args[2]
+		act = os.Args[1]
+		flagsIdx = 3
+	case len(os.Args) >= 2 && !isFlag(os.Args[1]):
+		act = os.Args[1]
+		flagsIdx = 2
+	}
+
+	var object *Object
+	var action *Action
+	var ok bool
+
+	if object, ok = app.objects[obj]; !ok {
+		fmt.Println("object does not exist")
+		fmt.Println(app.Usage())
+		os.Exit(0)
+	}
+
+	if action, ok = object.actions[act]; !ok {
+		fmt.Println("action does not exist")
+		fmt.Println(app.Usage())
+		os.Exit(0)
+	}
+
+	if len(os.Args) > flagsIdx {
+		action.fs.Parse(os.Args[flagsIdx:])
+	}
+
+	if err := action.Run(action.fs); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
 
 func isFlag(s string) bool {
